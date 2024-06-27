b170e92091deca4d9e09e6cda48f055c04905a3b
WooJin <106531904+h117562@users.noreply.github.com>
2024-06-28 03:10:38 +0900

    [Add] Backup

---
+++ Tutorial Assimp/Mesh.h
@@ -0,0 +1,41 @@
+#ifndef _MESH_H_
+#define _MESH_H_
+
+#include <vector>
+#include <string>
+#include <d3dx11.h>
+#include <DirectXMath.h>
+
+using namespace std;
+using namespace DirectX;
+
+struct Vertex
+{
+	DirectX::XMFLOAT3 position;
+	DirectX::XMFLOAT2 textureCoord;
+};
+
+class Mesh
+{
+public:
+	Mesh();
+
+	bool InitializeBuffer(ID3D11Device* pDevice);
+	void Render(ID3D11DeviceContext* pDeviceContext);
+	void Shutdown();
+
+	void SetResource(ID3D11Device* pDevice, std::string filePath);
+public:
+	std::vector<Vertex> vertices;
+	std::vector<UINT> indices;
+	ID3D11ShaderResourceView* diffuseTexture;
+
+private:
+	ID3D11Buffer* m_vertexBuffer;
+	ID3D11Buffer* m_indexBuffer;
+
+	UINT stride = sizeof(Vertex);
+	UINT offset = 0;
+};
+
+#endif
